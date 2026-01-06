1071153afa1d9b274749c4a1da6e67a7c9f0f170
myzayy <116192354+myzayy@users.noreply.github.com>
2026-01-06 16:53:49 +0100

    Staging to Star Schema transformation and Visualization
    Staging to Star Schema transformation and Dashboard sql visualizations from Snowflake

---
+++ sql/bank.sql
@@ -0,0 +1,149 @@
+--USE DATABASE BANKING_RAW;
+--USE SCHEMA INSIGHTS;
+
+
+--SELECT * FROM FDIC_INSTITUTIONS 
+--LIMIT 5;
+
+--SELECT * FROM FRED_FINANCIAL_LABOR_PERFORMANCE 
+--LIMIT 5;
+
+
+-- SETUP
+CREATE DATABASE IF NOT EXISTS US_BANKING_DB;
+USE DATABASE US_BANKING_DB;
+
+CREATE SCHEMA IF NOT EXISTS STAGING; -- raw copies
+CREATE SCHEMA IF NOT EXISTS DWH;     -- completed star schema
+
+
+
+--SELECT * FROM BANKING_RAW.INSIGHTS.FDIC_INSTITUTIONS 
+--LIMIT 10;
+
+-- ============================================================
+
+-- STAGING
+USE SCHEMA STAGING;
+
+CREATE OR REPLACE TABLE stg_fdic_institutions (
+
+    stg_id INT IDENTITY(1,1) PRIMARY KEY, 
+    bank_name   STRING,
+    city        STRING,
+    state       STRING,
+    asset_amt   INT, -- money
+    deposit_amt INT,
+    est_date    DATE,
+    website     STRING
+);
+
+INSERT INTO stg_fdic_institutions (bank_name, city, state, asset_amt, deposit_amt, est_date, website)
+SELECT 
+    MSA,        -- bank name
+    CONSERVE,   -- city
+    STALP,      -- state
+    ADDRESS, -- assets
+    DEPDOM,  -- deposits
+    EQ,        -- date of found
+    WEBADDR     -- site
+FROM BANKING_RAW.INSIGHTS.FDIC_INSTITUTIONS
+WHERE MSA IS NOT NULL 
+  AND TRY_TO_NUMBER(ADDRESS) > 0;
+
+SELECT * FROM stg_fdic_institutions;
+SELECT count(*) FROM stg_fdic_institutions;
+
+-- star schema Transform n load
+-- DIMENSION
+USE SCHEMA DWH;
+
+-- geography
+CREATE OR REPLACE TABLE DIM_GEO (
+    geo_id INT IDENTITY(1,1) PRIMARY KEY,
+    state_code STRING,
+    city_name STRING
+);
+
+INSERT INTO DIM_GEO (state_code, city_name)
+SELECT DISTINCT
+    state,
+    city
+FROM STAGING.STG_FDIC_INSTITUTIONS WHERE state IS NOT NULL
+ORDER BY state, city;
+
+SELECT * FROM dim_geo LIMIT 5;
+
+--Bank details
+CREATE OR REPLACE TABLE DIM_BANK_DETAILS (
+    bank_id INT PRIMARY KEY,
+    name STRING,
+    website STRING
+);
+
+INSERT INTO DIM_BANK_DETAILS (bank_id, name, website)
+SELECT
+    stg_id,
+    bank_name,
+    website
+FROM STAGING.STG_FDIC_INSTITUTIONS;
+
+SELECT * FROM dim_bank_details LIMIT 5;
+
+--Date
+CREATE OR REPLACE TABLE DIM_DATE (
+    date_id INT IDENTITY(1,1) PRIMARY KEY,
+    full_date DATE,
+    year INT,
+    month INT,
+    quarter INT
+);
+
+INSERT INTO DIM_DATE (full_date, year, month, quarter)
+SELECT
+    est_date,
+    YEAR(est_date),
+    MONTH(est_date),
+    QUARTER(est_date)
+FROM STAGING.STG_FDIC_INSTITUTIONS WHERE est_date IS NOT NULL
+GROUP BY EST_DATE
+ORDER BY EST_DATE;
+
+SELECT * FROM DIM_DATE LIMIT 5;
+
+-- facts
+
+CREATE OR REPLACE TABLE fact_bank_perfomance (
+    fact_id INT IDENTITY(1, 1) PRIMARY key,
+    bank_id_fk INT,
+    geo_id_fk INT,
+    established_date_id_fk INT,
+    total_assets INT,
+    total_deposits INT,
+    rank_in_state INT,
+    avg_state_assets INT
+);
+
+INSERT INTO fact_bank_perfomance (
+    bank_id_fk, geo_id_fk, established_date_id_fk, total_assets, total_deposits, rank_in_state, avg_state_assets
+)
+SELECT
+    b.bank_id,
+    g.geo_id,
+    d.date_id,
+    s.asset_amt,
+    s.deposit_amt,
+    RANK() OVER (PARTITION BY s.state ORDER BY s.asset_amt DESC),
+    AVG(s.asset_amt) OVER (PARTITION BY s.state)
+FROM
+    STAGING.stg_fdic_institutions s
+JOIN DIM_BANK_DETAILS b ON s.stg_id = b.bank_id
+LEFT JOIN DIM_GEO g ON s.state = g.state_code and s.city = g.city_name
+LEFT JOIN DIM_DATE d ON s.est_date = d.full_date;
+
+SELECT * FROM fact_bank_perfomance LIMIT 5;
+
+
+--cleaning
+USE SCHEMA STAGING;
+DROP TABLE IF EXISTS stg_fdic_institutions;
---
+++ sql/dashboard_visualizations.sql
@@ -0,0 +1,41 @@
+-- Finance map of USA
+SELECT 
+    g.state_code AS State,
+    SUM(f.total_assets) AS Total_Assets
+FROM fact_bank_perfomance f
+JOIN DIM_GEO g ON f.geo_id_fk = g.geo_id
+GROUP BY g.state_code
+ORDER BY Total_Assets DESC
+LIMIT 15;
+
+-- 10 Richest banks
+SELECT b.name as Bank_name,
+SUM(f.total_assets) as Total_assets
+From FACT_BANK_PERFOMANCE f
+JOIN dim_bank_details b ON f.bank_id_fk = b.bank_id
+GROUP BY b.name ORDER BY TOTAL_ASSETS DESC LIMIT 10;
+
+-- Bank system history
+SELECT d.year AS Year_of_Found, COUNT(f.bank_id_fk) AS New_Banks_account
+FROM fact_bank_perfomance f
+JOIN dim_date d ON f.established_date_id_fk = d.date_id
+WHERE d.year >= 1800 and d.year <= 2025
+GROUP BY d.year
+ORDER By d.year;
+
+-- Assets VS Deposits
+SELECT f.total_assets AS Assets,
+    f.total_deposits AS Deposits,
+    g.state_code AS State
+FROM fact_bank_perfomance f
+JOIN dim_geo g ON f.geo_id_fk = g.geo_id
+WHERE f.total_assets < 100000
+LIMIT 1000;
+
+-- 10 states by avg bank size
+SELECT DISTINCT g.state_code AS State,
+f.avg_state_assets AS Average_bank_size
+FROM fact_bank_perfomance f
+JOIN dim_geo g ON f.geo_id_fk = g.geo_id
+order by Average_bank_size DESC
+LIMIT 10;
